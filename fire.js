59d263954580c63dd6a151421ba17a288fbcb212
othonalberto <othonalberto@gmail.com>
2019-02-04 16:19:06 -0200

    First commit

---
+++ fire.js
@@ -0,0 +1,93 @@
+const canvasWidth = 50;
+const canvasHeight = 50;
+const firePixelsData = []
+const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
+const debug = false
+
+function createFireStructure() {
+    const numberOfPixels = canvasHeight * canvasWidth
+
+    for (let index = 0; index < numberOfPixels; index++) {
+        firePixelsData[index] = 0
+    }
+}
+
+function createFireSource() {
+    const floor = (canvasHeight * canvasWidth) - canvasWidth;
+
+    for (let column = 0; column < canvasWidth; column++) {
+        firePixelsData[floor + column] = 36
+    }
+}
+
+function calculateFirePropagation() {
+    var index = 0
+
+    for (let column = 0; column < canvasWidth; column++) {
+        for (let row = 0; row < canvasHeight; row++) {
+            index = column + (canvasWidth * row)
+
+            updateFireIntensity(index);
+        }
+    }
+
+    render();
+}
+
+function updateFireIntensity(index) {
+    const bellow = index + canvasWidth;
+
+    if (bellow >= canvasHeight * canvasWidth ) return
+
+    const decay = Math.floor(Math.random() * 3) 
+
+    const newIntensity = firePixelsData[bellow] - decay
+
+    const newIndex = index - decay
+    
+    firePixelsData[newIndex] = newIntensity >= 0 ? newIntensity : 0
+}
+
+function render() {
+    var pixelIndex = 0
+    var pixelIntensity = 0
+
+    var html = `<table cellpadding=0 cellspacing=0>`
+
+    for (let row = 0; row < canvasHeight; row++) {
+        html += `<tr>`
+
+        for (let column = 0; column < canvasWidth; column++) {
+            pixelIndex = column + (canvasWidth * row);
+            pixelIntensity = firePixelsData[pixelIndex];
+
+            if (debug) {
+                html += `<td>`
+                html += `<div class="indexDisplay">${pixelIndex}</div>`
+                html += pixelIntensity
+                html += `</td>`
+            } else {
+                const color = fireColorsPalette[pixelIntensity]
+                const colorRGB = `${color.r}, ${color.g}, ${color.b}`
+                
+                html += `<td class="pixel" style="background-color: rgb(${colorRGB})"`
+                html += `</td>`
+            }
+        }
+
+        html += `</tr>`
+    }
+
+    html += `</table>`
+
+    document.querySelector(`#fire-div`).innerHTML = html;
+}
+
+function start() {
+    createFireStructure();
+    createFireSource();
+    
+    setInterval(calculateFirePropagation, 50);
+}
+
+start();
